d34cd65bfd1074632508a294b6ff0d604276df10
Chad Furman <chad@fuzzproductions.com>
2015-05-29 00:24:16 -0400

    added comments angular

---
+++ assets/public/js/components/ct-comments/ctCommentsController.js
@@ -0,0 +1,2 @@
+(function (ctApp) {
+})(ctApp);
--- assets/public/js/main.js
+++ assets/public/js/main.js
@@ -1,8 +1,43 @@
+angular.module('ctApp', [])
+    .directive('ctComments', function () {
+        return {
+            restrict: 'EA', //Default in 1.3+
+            scope: {
+                datasource: '=',
+                add: '&'
+            },
+            controller: 'ctCommentsController',
+            templateUrl: '/js/components/ct-comments/ctComments.html',
+        };
+    })
+    .controller('ctCommentsController', ['$scope', '$http', function ($scope, $http) {
+        function init() {
+            $http({
+                method: 'GET',
+                url: 'http://localhost:3000/comments',
+                headers: 'Access: application/json'
+            }).success(function(response) {
+                $scope.comments = response.data;
+            });
+        }
 
-(function(window, $) {
-  'use strict';
+        init();
 
-  //$(function() {
-  //});
+        $scope.addComment = function () {
+            //Add new customer to directive scope
+            $scope.comments.push({
+                message: $scope.newMessage
+            });
 
-}(window, jQuery));
+            $http({
+                method: 'POST',
+                url: 'http://localhost:3000/comments',
+                data: {
+                    message: $scope.newMessage
+                },
+                headers: 'Access: application/json'
+            }).error(function(response) {
+                $scope.comments.pop();
+            });
+        };
+    }]);
